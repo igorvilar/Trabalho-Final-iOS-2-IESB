6fb8a794093359ff14cbf2182697e6f5eb818280
Sávio Thallis <sthallis@gmail.com>
2018-11-09 23:09:52 -0200

    DetalheLojaViewController UIViewController com uma TableView

---
+++ Projeto Lojas/View/Lojas/Cell/DetalhaProdutoTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DetalhaProdutoTableViewCell.swift
+//  Projeto Lojas
+//
+//  Created by Savio Silva on 09/11/18.
+//  Copyright © 2018 com.iesb. All rights reserved.
+//
+
+import UIKit
+
+class DetalhaProdutoTableViewCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
--- Projeto Lojas/View/Lojas/DetalheLojaViewController.swift
+++ Projeto Lojas/View/Lojas/DetalheLojaViewController.swift
@@ -8,10 +8,38 @@
 
 import UIKit
 
-class LojaDetalheViewController: UIViewController {
-
+class LojaDetalheViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    var items: [String] = ["Swift1", "Swift2", "Swift3", "Swift1", "Swift2", "Swift3"]
+    
+    @IBOutlet weak var imagemProduto: UIImageView!
+    @IBOutlet weak var tituloProduto: UILabel!
+    @IBOutlet weak var descricaoProduto: UILabel!
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "celulaIdentifier")
+        self.tableView.delegate = self;
+        self.tableView.dataSource = self;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "celulaIdentifier")! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
     }
 
 }
